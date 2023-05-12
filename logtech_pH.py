7339884317af7c360f06788930dbb5760f098883
plainsweet <130041312+plainsweet@users.noreply.github.com>
2023-05-12 14:21:38 +0700

    Update logtech_pH.py

--- logtech_pH.py
+++ logtech_pH.py
@@ -252,7 +252,7 @@ if test == "Home":
     ''')
 
             else:
-                st.error('Nilai yang anda masukkan salah, inputkan dalam range 3.1-10')
+                st.error('Nilai yang anda masukkan salah, inputkan dalam range 1-14')
 
     
         
